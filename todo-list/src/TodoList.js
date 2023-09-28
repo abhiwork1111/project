ffc4e28b8771f9ab52cc9c6e188ca95a80d7473b
abhiwork1111 <abhiwork1111@gmail.com>
2023-09-28 15:33:03 -0400

    present working react-todo-list

---
+++ todo-list/src/TodoList.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [tasks, setTasks] = useState([]);
+  const [taskText, setTaskText] = useState('');
+
+  const addTask = () => {
+    if (taskText.trim() !== '') {
+      setTasks([...tasks, { text: taskText, completed: false }]);
+      setTaskText('');
+    }
+  };
+
+  const toggleTaskStatus = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = !updatedTasks[index].completed;
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={taskText}
+          onChange={(e) => setTaskText(e.target.value)}
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleTaskStatus(index)}
+            />
+            <span className={task.completed ? 'completed' : ''}>{task.text}</span>
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
